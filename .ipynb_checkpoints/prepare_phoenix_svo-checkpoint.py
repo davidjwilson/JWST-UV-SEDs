c21586c941d4bb6e6f7c0bc00a4aec919ad744f6
davidjwilson <djwilson394@gmail.com>
2022-11-04 17:43:54 -0600

    more tau ceti

--- .ipynb_checkpoints/prepare_phoenix_svo-checkpoint.py
+++ .ipynb_checkpoints/prepare_phoenix_svo-checkpoint.py
@@ -11,6 +11,8 @@ import astropy.constants as const
 from scipy.interpolate import griddata, interp1d
 import sys
 import glob
+from astropy.convolution import convolve_fft
+from astropy.convolution import Gaussian1DKernel
 
 
 
@@ -261,6 +263,57 @@ def find_normfac(radius, distance):
     return (radius.to(u.cm)/distance.to(u.cm))**2
  
 
+def smear(w,f, R, w_sample=1):
+    '''
+    Smears a model spectrum with a gaussian kernel to the given resolution, R.
+    Adapeted from https://github.com/spacetelescope/pysynphot/issues/78
+
+    Parameters
+    -----------
+
+    w,f:  spectrum to smear
+
+    R: int
+        The resolution (dL/L) to smear to
+
+    w_sample: int
+        Oversampling factor for smoothing
+
+    Returns
+    -----------
+
+    sp: PySynphot Source Spectrum
+        The smeared spectrum
+    '''
+
+    # Save original wavelength grid and units
+    w_grid = w
+    
+
+    # Generate logarithmic wavelength grid for smoothing
+    w_logmin = np.log10(np.nanmin(w_grid))
+    w_logmax = np.log10(np.nanmax(w_grid))
+    n_w = np.size(w_grid)*w_sample
+    w_log = np.logspace(w_logmin, w_logmax, num=n_w)
+
+    # Find stddev of Gaussian kernel for smoothing
+    R_grid = (w_log[1:-1]+w_log[0:-2])/(w_log[1:-1]-w_log[0:-2])/2
+    sigma = np.median(R_grid)/R
+    if sigma < 1:
+        sigma = 1
+
+    # Interpolate on logarithmic grid
+    f_log = np.interp(w_log, w_grid, f)
+
+    # Smooth convolving with Gaussian kernel
+    gauss = Gaussian1DKernel(stddev=sigma)
+    f_conv = convolve_fft(f_log, gauss)
+
+    # Interpolate back on original wavelength grid
+    f_sm = np.interp(w_grid, w_log, f_conv)
+
+    # Write smoothed spectrum back into Spectrum object
+    return w_grid, f_sm
 
 def test():
     star = 'Trappist-1_test' 
@@ -299,6 +352,8 @@ def test2():
     star_params = {'Teff': teff, 'logg': np.log10(g.value), 'FeH': 0.00, 'aM': 0, 'Radius':radius*u.R_sun, 'Distance':distance*u.pc, 'Teff_e':teff_e}
     pw, pf, pe = make_phoenix_spectrum(star, save_path, repo, star_params, save_ecsv=False, plot=False, make_error=True)
     normfac = ((radius*R)/(distance*u.pc.to(u.m)))**2
+
+    
 
 
 # test2()
