26ef4bb5f3f29835ca107aac300ffb347f1c4bf3
davidjwilson <djwilson394@gmail.com>
2023-01-26 18:17:07 -0700

    wasp 63 v1 done

---
+++ sed_scripts/.ipynb_checkpoints/make_fits-checkpoint.py
@@ -0,0 +1,138 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.io.fits as fits
+from astropy.table import Table
+from astropy.io import ascii
+import astropy.units as u
+from astropy.units import cds
+import instruments
+cds.enable()
+"""
+Making hslp standard fits files for Mega-Muscles
+"""
+def make_primary_header(hdr, sed_table, instrument_list):
+    meta = sed_table.meta
+    telescopes = [instruments.getinststr(inst)[0:3] for inst in instrument_list]
+    instrus = [instruments.getinststr(inst)[4:7] for inst in instrument_list]
+    gratings = [instruments.getinststr(inst)[8:] for inst in instrument_list]
+    hdr.append(('TELESCOP', 'MULTI'))
+    hdr.append(('INSTRUME', 'MULTI'))
+    hdr.append(('GRATING', 'MULTI'))
+    for i in range(len(telescopes)):
+        hdr.append(('TELESC{:02.0f}'.format(i), telescopes[i].upper()))
+        hdr.append(('INSTRU{:02.0f}'.format(i), instrus[i].upper()))
+        hdr.append(('GRATIN{:02.0f}'.format(i), gratings[i].upper()))
+    extra_keys =  ['TARGNAME','RA_TARG','DEC_TARG','PROPOSID','HLSPNAME','HLSPACRN','HLSPLEAD','PR_INV_L',
+                   'PR_INV_F','WAVEMIN','WAVEMAX','WAVEUNIT','AIRORVAC','FLUXMIN',
+                  'FLUXMAX','FLUXUNIT', 'BOLOFLUX', 'LNZ_NORM','LNZ_GAM']
+    for key in extra_keys[:-2]: #temporary while I don't have the last ones in there.
+        hdr.append((key,meta[key]))
+    return hdr
+
+
+
+def data_header(hdr):
+    """
+    Makes the header for the table extension. 
+    
+    Keywords to add from MUSCLES:
+
+    TDESC1  = 'midpoint of the wavelength bin'                                      
+    TDESC2  = 'left/blue edge of the wavelength bin'                                
+    TDESC3  = 'right/red edge of the wavelength bin'                                
+    TDESC4  = 'average flux over the bin'                                           
+    TDESC5  = 'error on the flux'                                                   
+    TDESC6  = 'cumulative exposure time for the bin'                                
+    TDESC7  = 'data quality flags (HST data only)'                                  
+    TDESC8  = 'modified julian date of start of first exposure'                     
+    TDESC9  = 'modified julian date of end of last exposure'                        
+    TDESC10 = 'bitmask identifying the source instrument(s). See "instlgnd" & extension for a legend.'                                             
+    TDESC11 = 'normalization factor applied to the source spectrum'                 
+    TDESC12 = 'flux density normalized by the bolometric flux'                      
+    TDESC13 = 'error on bolometrically-normalized flux density'
+
+    """
+    new_keywords = ('TDESC1','TDESC2','TDESC3','TDESC4', 'TDESC5',
+               'TDESC6', 'TDESC7', 'TDESC8', 'TDESC9', 'TDESC10',
+               'TDESC11','TDESC12','TDESC13') 
+    new_values =  ('midpoint of the wavelength bin',
+                   'left/blue edge of the wavelength bin',
+                   'right/red edge of the wavelength bin',
+                   'average flux over the bin',
+                   'error on the flux',
+                   'cumulative exposure time for the bin',
+                   'data quality flags (HST data only)',
+                   'modified julian date of start of first exposure',
+                   'modified julian date of end of last exposure',
+                   'bitmask identifying the source instrument(s). See "instlgnd" & extension for a legend.',
+                   'normalization factor applied to the source spectrum',
+                   'flux density normalized by the bolometric flux',
+                   'error on bolometrically-normalized flux density')
+    for i, n, v in zip(range(len(new_keywords)), new_keywords, new_values):
+        hdr.insert(i+8, (new_keywords[i], new_values[i]))
+    
+    return hdr
+
+
+def make_instrument_extension(sed_table, instlist, version):
+    """
+    Make the instrument extension for a MEGA-MUSCLES file, containing translations of the instrument codes and the list of component file names
+    
+    """
+    telescopes = [instruments.getinststr(inst)[0:3].upper() for inst in instlist]
+    instrus = [instruments.getinststr(inst)[4:7].upper() for inst in instlist]
+    gratings = [instruments.getinststr(inst)[8:].replace('-----','na').upper() for inst in instlist]
+    target = sed_table.meta['TARGNAME']
+    filenames = ['hlsp_muscles_{}_{}_{}_{}_v{}_component-spec.fits'.format(tel, inst, target, grating, version).lower() for tel, inst, grating in zip(telescopes, instrus,gratings)]
+    data = Table([instlist, telescopes, instrus, gratings , filenames], names = ['BITVALUE','TELESCOPE','INSTRUMENT','GRATING','HLSP_FILE'])
+    hdu = fits.table_to_hdu(data)
+    
+    comment = 'This extension is a legend for the integer identifiers in the instrument column of the '\
+              'spectrum extension. Instruments are identified by bitwise flags so that any combination of '\
+              'instruments contributing to the data within a spectral element can be identified together. '\
+              'For example, if instruments 4 and 16 (100 and 10000 in binary) both contribute to the data '\
+              'in a bin, then that bin will have the value 20, or 10100 in binary, to signify that both '\
+              'instruments 4 and 16 have contributed. This is identical to the handling of bitwise data '\
+              'quality flags. Note that polynomial fits for filling spectral gaps were not saved as separate spectra.'\
+
+    
+    
+    hdu.header.append(('COMMENT',comment))
+    hdu.name = 'INSTLGND'
+    return hdu
+    
+    
+
+
+def make_data_ext(sed_table):
+    """
+    The table extension, takes an astropy table 
+    """
+    hdu = fits.table_to_hdu(Table(dict(sed_table)))
+    hdu.header = data_header(hdu.header)
+    hdu.name = 'SPECTRUM' 
+    return hdu
+
+
+def make_primary_ext(sed_table, instrument_list):
+    """
+    Make the primary header
+    """
+    primary_hdu = fits.PrimaryHDU()
+    primary_hdu.header = make_primary_header(primary_hdu.header, sed_table, instrument_list)
+    return primary_hdu
+
+def make_mm_fits(savepath, sed_table, instrument_list, version,sed_type='var'):
+    """
+    Saves an SED as a Mega-MUSCLES fits file
+    """
+    primary_hdu = make_primary_ext(sed_table, instrument_list)
+    data_ext = make_data_ext(sed_table)
+    inst_ext = make_instrument_extension(sed_table, instrument_list, version)
+    star = sed_table.meta['TARGNAME'].lower()
+    if star == '2mass-j23062928-0502285':
+        star = 'trappist-1'
+    file_name = 'hlsp_muscles_multi_multi_{}_broadband_v{}_{}-res-sed'.format(star, version, sed_type)
+    hdul = fits.HDUList([primary_hdu, data_ext,inst_ext])
+    hdul.writeto('{}{}.fits'.format(savepath,file_name), overwrite=True)
+    print('sed saved as {}'.format(file_name))
---
+++ sed_scripts/sed_input.py
@@ -0,0 +1,221 @@
+"""
+@verison: 5
+
+@author: David Wilson
+
+@date 20230126
+
+Organises all of the Mega-MEATS data and produces the SEDs. Updated for MEATS from the MM script.
+"""
+
+import instruments
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import astropy.io.fits as fits
+import os
+from scipy.io.idl import readsav
+from astropy.table import Table, vstack
+from astropy.io import ascii
+import astropy.units as u
+import make_mm_sed as sed
+# import prepare_cos
+# import prepare_stis
+# import prepare_model
+# import prepare_xmm
+# import prepare_euv
+# import prepare_chandra
+from craftroom import resample
+from scipy.interpolate import interp1d
+import make_sed_files
+from shutil import copyfile
+import make_fits
+import instruments
+
+"""File structure"""
+
+# path = '/media/david/5tb_storage1/muscles/' #data are in the external harddrive
+# path = '/media/david/1tb_storage1/emergency_data/mega_muscles/' #backup hd
+path = '/media/david/2tb_ext_hd/hddata/mega_muscles/' #new hd
+sources = ['cos','stis', 'lya','phoenix', 'xmm', 'chandra', 'apec', 'euv']
+
+
+stars = ['WASP-63']
+
+airglow =  [1207, 1222, 1300, 1310, 1353, 1356]
+cos_gratings = ['G130M', 'G160M']
+stis_gratings = ['G140M','E140M','G140L', 'G230L', 'G230LB', 'G430L']
+
+#lya_ecsvs = glob.glob('{}*ecsv'.format(lya_path))
+
+#print(lya_ecsvs)
+###################
+version = 10
+###################
+
+def make_repo(star, path, version):
+    """
+    Makes directories to store the produced files
+    """
+    repo = '{}hlsp/{}/'.format(path, star)
+    component_repo = '{}components_v1/'.format(repo)
+    if os.path.exists(repo) == False: #makes the parent directory then puts another directory in it for the components
+        os.mkdir(repo)
+    if os.path.exists(component_repo) == False:
+        os.mkdir(component_repo)
+    return repo, component_repo
+
+def sort_components(star, path, sources, component_repo):
+    """
+    Moves all components to individual star directories
+    """
+    for source in sources:
+        spath = '{}{}_hlsp/'.format(path, source)
+        starfiles = glob.glob('{}*{}*'.format(spath, star.lower()))
+        for sf in starfiles:
+            fname = os.path.split(sf)[1]
+            copyfile(sf, '{}{}'.format(component_repo, fname))
+
+      #  print(spec)
+              #  filename = os.path.split(spec)[1]
+               # copyfile(spec, 'nuv_collection/spectra/{}'.format(filename))
+
+def which_xray(component_repo):
+    xscope = 'none'
+    if len(glob.glob('{}*xmm*'.format(component_repo))) > 0:
+           xscope = 'xmm'
+    if len(glob.glob('{}*cxo*'.format(component_repo))) > 0:
+           xscope = 'cxo'
+    return xscope 
+
+def table_name(star): 
+    """
+    Turns the input name into the name in the stellar params table
+    """
+    if star[:2] == 'GJ':
+        star = star.replace(' ', '')
+    elif star == 'Trappist-1':
+        star = '2MASS-J23062928-0502285'
+    elif star[0] == 'L':
+        star = star.replace(' ', '-')
+    return star
+    
+star_params_path = '/home/david/work/muscles/SEDs/optical/stellar_parameters.csv'
+star_params = Table.read(star_params_path)
+targets = np.array([table_name(star) for star in star_params['Target']])
+# print(targets)
+ 
+def make_mm_seds(path, star_params, stars, version, norm=False, remove_negs=False, to_1A=False, sed_type='adapt-var'): 
+    
+    for star in stars:
+        print(star)
+        repo, component_repo = make_repo(star, path, version)
+        print(component_repo)
+    #     sort_components(star, path, sources, component_repo)
+
+        row = star_params[np.where(targets == star)[0][0]]
+
+       #COS
+
+        sed_table, instrument_list = sed.add_cos(component_repo, airglow, remove_negs=remove_negs, to_1A=to_1A)
+
+        #STIS and Lya
+
+        sed_table, instrument_list = sed.add_stis_and_lya(sed_table, component_repo, airglow[0:2], instrument_list, airglow[2:], norm=False, remove_negs=remove_negs,to_1A=to_1A)
+
+        #PHOENIX
+
+
+
+        sed_table, instrument_list = sed.add_phoenix_and_g430l(sed_table, component_repo, instrument_list, remove_negs=remove_negs, to_1A=to_1A)
+    #     sed_table, instrument_list= sed.add_phx_spectrum(sed_table, component_repo, instrument_list)
+
+        #X-ray
+
+        sed_table, instrument_list, euv_gap = sed.add_xray_spectrum(sed_table, component_repo, instrument_list, which_xray(component_repo), add_apec = True, find_gap=True,to_1A=to_1A)
+
+        #EUV
+        euv_name = 'euv-scaling'
+        if len(glob.glob('{}*dem*'.format(component_repo))) > 1:
+            euv_name = 'dem'
+
+        sed_table, instrument_list = sed.add_euv(sed_table, component_repo, instrument_list, euv_gap, euv_name,to_1A=to_1A)
+
+        sed_table.sort(['WAVELENGTH'])
+    #     print(sed_table.meta)
+        #bolometric flux
+        sed_table = sed.add_bolometric_flux(sed_table, component_repo, row)
+
+        print(sed_table['BOLOFLUX'][100])
+
+        #final meta keys
+        sed_table.meta['WAVEMIN'] = min(sed_table['WAVELENGTH'])
+        sed_table.meta['WAVEMAX'] = max(sed_table['WAVELENGTH'])
+        sed_table.meta['FLUXMIN'] = min(sed_table['FLUX'])
+        sed_table.meta['FLUXMAX'] = max(sed_table['FLUX'])
+
+        # print('testing wavelength')
+        # test_w = np.arange(min(sed_table['WAVELENGTH']), max(sed_table['WAVELENGTH'])+1, 1)
+        # for wi in test_w:
+        #     if wi not in sed_table['WAVELENGTH']:
+        #         print (wi)
+        # print('end of test')
+
+
+
+    #     np.save('test_to_fits/ti_instlist', instrument_list)
+    #     sed_table.write('test_to_fits/t1_table_test.ecsv', overwrite=True)
+        make_fits.make_mm_fits(component_repo, sed_table, instrument_list, version,sed_type=sed_type)
+    
+#     sed_table_1A = sed.sed_to_const_res(sed_table)
+#     sed_table_1A.meta['WAVEMIN'] = min(sed_table_1A['WAVELENGTH'])
+#     sed_table_1A.meta['WAVEMAX'] = max(sed_table_1A['WAVELENGTH'])
+#     sed_table_1A.meta['FLUXMIN'] = min(sed_table_1A['FLUX'])
+#     sed_table_1A.meta['FLUXMAX'] = max(sed_table_1A['FLUX'])
+#     print(min(sed_table_1A['WAVELENGTH']), max(sed_table_1A['WAVELENGTH']),min(sed_table_1A['FLUX']), max(sed_table_1A['FLUX']))
+#     make_fits.make_mm_fits(component_repo, sed_table_1A, instrument_list, version,sed_type='const')
+    
+#     print (sed_table.dtype.names)
+    
+#     print(sed_table[1300])
+#     print(sed_table.meta)
+#     print(instrument_list)
+ 
+#     savdat = Table([sed_table['WAVELENGTH']*u.AA, sed_table['FLUX']*u.erg/u.s/u.cm**2/u.AA, sed_table['ERROR']*u.erg/u.s/u.cm**2/u.AA], names=['WAVELENGTH', 'FLUX', 'ERROR'])
+#     ascii.write(savdat, '{}/basic_seds/{}_basic_v1.ecsv'.format(path, star), format='ecsv', overwrite=True)
+
+#     plt.figure(star, figsize=(7, 5))
+#     plt.step(sed_table['WAVELENGTH'], sed_table['FLUX'], label=star, where='mid')
+#     # plt.plot(sed_table['WAVELENGTH'], sed_table['ERROR'])
+# #     plt.plot(sed_table_1A['WAVELENGTH'], sed_table_1A['FLUX'], label=star)
+# #     plt.plot(sed_table_1A['WAVELENGTH'], sed_table_1A['ERROR'])
+
+# #     plt.ylim(lim)
+# #     plt.ylim(1e-17, 1e-13)
+# #     plt.xlim(5, 3e5)
+#     plt.yscale('log')
+#     plt.xscale('log')
+#     plt.xlabel('Wavelength (\AA)')
+#     plt.ylabel('Flux (erg s$^{-1}$cm$^{-2}$\AA$^{-1}$)')
+# #     plt.legend(loc=1)
+#     plt.tight_layout()
+#     #plt.savefig('plots/first_seds/{}_v{}_sed.png'.format(star, version))
+#     plt.show()
+#     plt.close()
+    
+    
+make_mm_seds(path, star_params, stars, version, norm=False, remove_negs=False, to_1A=False, sed_type='var')
+make_mm_seds(path, star_params, stars, version, norm=False, remove_negs=True, to_1A=False, sed_type='adapt-var')
+make_mm_seds(path, star_params, stars, version, norm=False, remove_negs=True, to_1A=True, sed_type='adapt-const')
+make_mm_seds(path, star_params, stars, version, norm=False, remove_negs=False, to_1A=True, sed_type='const')
+    
+
+print('Done')
+
+"""    
+
+    #savdat = Table([w_full*u.AA, f_full*u.erg/u.s/u.cm**2/u.AA, e_full*u.erg/u.s/u.cm**2/u.AA], names=['WAVELENGTH', 'FLUX', 'ERROR'])
+    #ascii.write(savdat, 'uv_first_pass/{}_hst+opt_v1.ecsv'.format(star), format='ecsv', overwrite=True)
+
+"""
+
