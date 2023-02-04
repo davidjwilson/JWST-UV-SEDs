3cb934b1553497b7f14a78b38d1793b147c8662b
davidjwilson <djwilson394@gmail.com>
2023-02-03 18:08:25 -0700

    prepare stis major update

--- .ipynb_checkpoints/prepare_stis-checkpoint.py
+++ .ipynb_checkpoints/prepare_stis-checkpoint.py
@@ -8,7 +8,7 @@ from astropy.io import ascii
 import astropy.units as u
 from scipy.interpolate import interpolate
 from astropy.units import cds
-from scipy.io.idl import readsav
+import astropy.stats as stats
 
 
 
@@ -17,12 +17,12 @@ cds.enable()
 """
 @author: David Wilson
 
-version 4 20221003
+version 5 20230203
 
 
 Finds all STIS x1d files, groups them by grating, coadds them and saves to file with required metadata. V3 fixes not knowning the exptimes for e140m
 
-V4 updates for Mega-Meats.
+V5 updated to make it more adapatable to different programs and new error corrections
 
 """
 def coadd_flux(f_array, e_array, scale_correct=True):
@@ -40,16 +40,26 @@ def coadd_flux(f_array, e_array, scale_correct=True):
         error = var**2
     return flux,error
 
-
 def no_zero_errors(flux, error):
     """
-    Corrects instances where negative flux measurements have very small errors
+    Corrects instances where negative flux measurements have very small errors. Superceeded by make_person_errors
     """
     e_new = error
     for i in range(len(error)):
         if flux[i] < 0.0 and error[i] < 0.1*abs(flux[i]):
             e_new[i] = abs(flux[i])
     return e_new
+
+def make_person_errors(data, hdr):
+    """
+    Recalculates the error array using a pearson confidence interval - this corrects for the pipeline producing errors with zero
+    """
+    sensitivity = data['FLUX'] / data['NET'] #sensitivity curve of the spectrum 
+    counts = data['GROSS'] * hdr['EXPTIME'] #total counts obtained in each wavelength bin during the exposure
+    ci = stats.poisson_conf_interval(counts, interval='pearson') #pearson confidence interval
+    count_errors = np.mean(abs(ci -counts), axis = 0) #average the upper and lower errorbars
+    new_error = count_errors * sensitivity/hdr['EXPTIME'] #convert error to flux units.
+    return new_error
 
 def build_wavelength(x1ds):
     """
@@ -66,7 +76,6 @@ def build_wavelength(x1ds):
     w_new = np.arange(min(starts),max(ends), max(diffs))
     return w_new
 
-
 def wavelength_edges(w):
     """
     Calulates w0 and w1
@@ -81,11 +90,8 @@ def nan_clean(array):
     """
     replace nans in arrays with zeros
     """
-    for i in range(len(array)):
-        if np.isnan(array[i]) == True:
-            array[i] = 0.0
+    array = np.nan_to_num(array, nan=0.0)
     return array
-
  
 def get_ayres_e140m(x1ds): 
     """ 
@@ -129,11 +135,9 @@ def combine_x1ds(x1ds, correct_error=True):
             end = []
             w_new = build_wavelength(x1ds)
             for x in x1ds:
-                data_extension = 1
-                if x[-8:-5] == 'sx1':
-                    data_extension = 0
-                data = fits.getdata(x,data_extension)[0]
+                data = fits.getdata(x, 1)[0]
                 hdr = fits.getheader(x,0)
+                hdr1 = fits.getheader(x,1)
                 fi = interpolate.interp1d(data['WAVELENGTH'], data['FLUX'], bounds_error=False, fill_value=0.)(w_new)
                 ei = interpolate.interp1d(data['WAVELENGTH'], data['ERROR'], bounds_error=False, fill_value=0.)(w_new)
                 dqi =  interpolate.interp1d(data['WAVELENGTH'], data['DQ'], kind='nearest',bounds_error=False, fill_value=0.)(w_new)
@@ -145,7 +149,8 @@ def combine_x1ds(x1ds, correct_error=True):
                 endi = interpolate.interp1d(data['WAVELENGTH'], endi, kind='nearest',bounds_error=False, fill_value=0.)(w_new)
 
                 if correct_error:    
-                    ei = no_zero_errors(fi, ei)
+                    # ei = no_zero_errors(fi, ei)
+                    ei = make_person_errors(data, hdr1)
                 f_new.append(fi)
                 e_new.append(ei)
                 dq_new.append(dqi)
@@ -169,8 +174,6 @@ def combine_x1ds(x1ds, correct_error=True):
         #else:
         data = fits.getdata(x1ds[0],data_extension)[0]   
         hdr = fits.getheader(x1ds[0],0)
-        if hdr['TARGNAME'] == '2MASS-J23062928-0502285':
-            data = Table.read('/home/david/work/muscles/SEDs/trappist-1/optical/t1_g430m_edit.ecsv')
         w_new, f_new, e_new, dq_new = data['WAVELENGTH'], data['FLUX'], data['ERROR'], data['DQ']
         exptime, start, end = np.full(len(data['WAVELENGTH']), hdr['TEXPTIME']), np.full(len(data['WAVELENGTH']), hdr['TEXPSTRT']), np.full(len(data['WAVELENGTH']), hdr['TEXPEND'])
         if correct_error:    
@@ -186,7 +189,7 @@ def combine_x1ds(x1ds, correct_error=True):
 
 
  
-def make_metadata(x1ds, new_data, normfac, star):
+def make_metadata(x1ds, new_data, hlsp, normfac, star):
     """
     Makes the metadata for the ecsv files- eventually will be converted into the fits file
     """
@@ -208,11 +211,12 @@ def make_metadata(x1ds, new_data, normfac, star):
             starname = hdr['TARGNAME']
         else:
             starname = star
-    meats_name = 'MUSCLES Extension for Atmospheric Transmisson Spectroscopy'
+    # meats_name = 'MUSCLES Extension for Atmospheric Transmisson Spectroscopy'
     meta_names =  ['TELESCOP', 'INSTRUME','GRATING','APERTURE','TARGNAME','RA_TARG','DEC_TARG','PROPOSID','HLSPNAME','HLSPACRN','HLSPLEAD','PR_INV_L',
                    'PR_INV_F','DATE-OBS','EXPSTART','EXPEND','EXPTIME','EXPDEFN','EXPMIN','EXPMAX','EXPMED','NORMFAC','WAVEMIN','WAVEMAX','WAVEUNIT','AIRORVAC','SPECRES','WAVERES','FLUXMIN',
                   'FLUXMAX','FLUXUNIT']
-    meta_fill = ['','',hdr['OPT_ELEM'],'',starname,'','','',meats_name,'MEATS','David J. Wilson','','',min(dates),min(start_times),max(end_times),sum(exptimes),'SUM', 
+    meta_fill = ['','',hdr['OPT_ELEM'],'',starname,'','','',hlsp['PROGRAM'],hlsp['PROGRAMSHORT'],hlsp['HLSPLEAD'],
+                 '','',min(dates),min(start_times),max(end_times),sum(exptimes),'SUM', 
                 min(exptimes), max(exptimes), np.median(exptimes),normfac,wavelength[0], wavelength[-1],'ang','vac',specres,waveres,np.min(flux[np.isnan(flux)==False]), np.max(flux[np.isnan(flux)==False]),'erg/s/cm2/ang']
     metadata = {}
     for name, filler in zip(meta_names, meta_fill):
@@ -255,13 +259,13 @@ def save_to_ecsv(data, metadata, save_path, version):
     print('Spectrum saved as '+file_name+'.ecsv')
 
     
-def save_to_fits(data, metadata, dataset_hdu, savepath, version):
+def save_to_fits(data, metadata, hlsp, dataset_hdu, savepath, version):
     """
     Saves to a MUSCLES-standard fits file
     """
     if os.path.exists(savepath) == False:
         os.mkdir(savepath)
-    file_name = make_component_filename(metadata, version)
+    file_name = make_component_filename(metadata, version, hlsp)
     hdr = fits.Header(metadata)
     primary_hdu = fits.PrimaryHDU(header=hdr)
     hdu = fits.table_to_hdu(Table(data))
@@ -277,12 +281,14 @@ def save_to_fits(data, metadata, dataset_hdu, savepath, version):
     
 
 def plot_spectrum(data, metadata):
-    plt.figure('%s_%s' % (metadata['TARGNAME'],metadata['GRATING']))
-    plt.step(data['WAVELENGTH'], data['FLUX'], where='mid')
-    plt.step(data['WAVELENGTH'], data['ERROR'], where='mid')
-    plt.xlabel('Wavelength (\AA)', size=20)
-    plt.ylabel('Flux (erg s$^{-1}$ cm$^{-2}$ \AA$^{-1}$)', size=20)
-    plt.tight_layout()
+    fig, ax = plt.subplots()
+    ax.step(data['WAVELENGTH'], data['FLUX'], where='mid', label='FLUX')
+    ax.step(data['WAVELENGTH'], data['ERROR'], where='mid', alpha=0.5, label='ERROR')
+    ax.set_xlabel('Wavelength (\AA)', size=20)
+    ax.set_ylabel('Flux (erg s$^{-1}$ cm$^{-2}$ \AA$^{-1}$)', size=20)
+    ax.set_title('{}_{}'.format(metadata['TARGNAME'],metadata['GRATING']))
+    ax.legend()
+    fig.tight_layout()
     plt.show()
 
 def stis_clean(x1ds):
@@ -295,11 +301,12 @@ def stis_clean(x1ds):
             stis_x1ds.append(x)
     return stis_x1ds
 
-def make_component_filename(metadata, version):
+def make_component_filename(metadata, version, hlsp):
     """
     Construct the filename for a component spectrum ,eg "hlsp_muscles_hst_stis_gj176_g230l_v22_component-spec.fits"hlsp_muscles_hst_stis_gj176_g230l_v22_component-spec.fits
     """
-    filename = 'hlsp_muscles_%s_%s_%s_%s_v%s_component-spec' %(metadata['TELESCOP'].lower(), metadata['INSTRUME'].lower(), metadata['TARGNAME'].lower(), metadata['GRATING'].lower(), version) 
+    # filename = 'hlsp_muscles_%s_%s_%s_%s_v%s_component-spec' %(metadata['TELESCOP'].lower(), metadata['INSTRUME'].lower(), metadata['TARGNAME'].lower(), metadata['GRATING'].lower(), version) 
+    filename = 'hlsp_{}_{}_{}_{}_{}_v{}_component-spec'.format(hlsp['HLSPNAME'].lower(),metadata['TELESCOP'].lower(), metadata['INSTRUME'].lower(), metadata['TARGNAME'].lower(), metadata['GRATING'].lower(), version) 
     return filename
 
 def make_dataset_extension(x1ds):
@@ -322,51 +329,48 @@ def make_dataset_extension(x1ds):
     return hdu
 
     
-def make_stis_spectrum(x1dpath, version,savepath = '', plot=False, save_ecsv=False, save_fits=False, return_data=False, return_gratings = False, normfac=1.0, sx1 = True, star = ''):
+def make_stis_spectrum(x1dpath, version, hlsp, savepath = '', plot=False, save_ecsv=False, save_fits=False, return_data=False, return_gratings = False, normfac=1.0, star = ''):
     """
     main function
     """
-    all_x1ds = glob.glob(x1dpath+'*x1d.fits')
-    stis_x1ds = stis_clean(all_x1ds) #get rid of any not-stis x1ds
-    if sx1:
-        all_sx1 = glob.glob(x1dpath+'*sx1.fits') #do the ccd as well
-        stis_x1ds = np.concatenate((stis_x1ds, all_sx1))
-    
+    all_x1ds = np.hstack((glob.glob('{}*_x1d.fits'.format(x1dpath)), glob.glob('{}*sx1.fits'.format(x1dpath))))
+    stis_x1ds = stis_clean(all_x1ds)
+    hlsp = Table.read(hlsp)[0]
     if len(stis_x1ds) > 0:
         gratings, x1ds_by_grating = setup_list(stis_x1ds)
         for x1ds in x1ds_by_grating:
             data = combine_x1ds(x1ds)
            # data = [wavelength*u.AA, flux*u.erg/u.s/u.cm**2/u.AA, error*u.erg/u.s/u.cm**2/u.AA, dq]
-            metadata = make_metadata(x1ds, data, normfac, star)
+            metadata = make_metadata(x1ds, data, hlsp, normfac, star)
             if plot:
                 plot_spectrum(data, metadata)
             if save_ecsv:
                 save_to_ecsv(data, metadata, savepath, version)
             if save_fits:
                 data_set_hdu = make_dataset_extension(x1ds)
-                save_to_fits(data, metadata, data_set_hdu, savepath, version)
+                save_to_fits(data, metadata, hlsp, data_set_hdu, savepath, version)
     if return_data:
         return data
     if return_gratings:
         return gratings
 
 
-def test():
-    """
-    testing with GJ 699
-    """
-    x1dpath = '/home/david/work/muscles/MegaMUSCLES/GJ_699/HST/STIS/'
-    version = 2
-    savepath = 'test_files/'
-    make_stis_spectum(x1dpath, version, savepath = savepath, plot=True, save_ecsv=True, save_fits=True)
+# def test():
+#     """
+#     testing with GJ 699
+#     """
+#     x1dpath = '/home/david/work/muscles/MegaMUSCLES/GJ_699/HST/STIS/'
+#     version = 2
+#     savepath = 'test_files/'
+#     make_stis_spectum(x1dpath, version, savepath = savepath, plot=True, save_ecsv=True, save_fits=True)
     
-def e140m_redo():
-    stars = ['GJ15A', 'GJ729']
-    for star in stars:
-        x1dpath = '/media/david/1tb_storage1/emergency_data/mega_muscles/e140m_fits/{}/'.format(star)
-        version = 2
-        savepath = 'e140m_test/'
-        make_stis_spectum(x1dpath, version, savepath = savepath, plot=True, save_ecsv=True, save_fits=True)
+# def e140m_redo():
+#     stars = ['GJ15A', 'GJ729']
+#     for star in stars:
+#         x1dpath = '/media/david/1tb_storage1/emergency_data/mega_muscles/e140m_fits/{}/'.format(star)
+#         version = 2
+#         savepath = 'e140m_test/'
+#         make_stis_spectum(x1dpath, version, savepath = savepath, plot=True, save_ecsv=True, save_fits=True)
     
 # e140m_redo()
 
