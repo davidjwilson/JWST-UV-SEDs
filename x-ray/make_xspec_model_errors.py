bea955d3ad23ce44c6d1525bf7dd638304e31541
davidjwilson <djwilson394@gmail.com>
2025-02-12 17:38:12 -0700

    update

--- x-ray/make_xspec_model_errors.py
+++ x-ray/make_xspec_model_errors.py
@@ -15,7 +15,7 @@ xtab = Table.read('mMEATS_xray_models.csv')
 hlsppath = '../draft_hlsp/'
 
 for x in xtab:
-    if x['Star'] == 'WASP-166':
+    if x['Star'] == 'TOI-402':
         n = 0
         fluxes = []
         while n < ntries:
